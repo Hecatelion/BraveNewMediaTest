af346d32eea2fe5120f976c0f706c8b50127cf62
Baptiste <billard.baptiste.etudes@gmail.com>
2020-11-10 18:40:52 +0100

    AR project set up + android build test
    - quick ar features test : 
        - image traking
        - gameobject spawning on it
        - changing material on GO touch

---
+++ BNMUnityProject/Assets/Scripts/AutoCamRef.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCamRef : MonoBehaviour
+{
+	Camera cam;
+
+    void Start()
+    {
+		cam = GameObject.FindObjectOfType<Camera>();
+		Debug.Log(cam.name);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ BNMUnityProject/Assets/Scripts/UiOnClick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiOnClick : MonoBehaviour
+{
+	[SerializeField] Material selectedMat;
+	[SerializeField] Material notSelectedMat;
+
+	public GameObject ui;
+
+	public bool isSelected = false;
+
+    void Start()
+    {
+		ui = GetComponentInChildren<Canvas>().gameObject;
+		UpdateSelection();
+	}
+
+    void Update()
+    { }
+
+	private void OnMouseDown()
+	{
+		isSelected = !isSelected;
+		UpdateSelection();
+	}
+
+	private void UpdateSelection()
+	{
+		if (isSelected)
+		{
+			Select();
+		}
+		else
+		{
+			Unselect();
+		}
+	}
+
+	private void Select()
+	{
+		gameObject.GetComponent<MeshRenderer>().material = selectedMat;
+		ui.SetActive(true);
+	}
+	private void Unselect()
+	{
+		gameObject.GetComponent<MeshRenderer>().material = notSelectedMat;
+		ui.SetActive(false);
+	}
+}
