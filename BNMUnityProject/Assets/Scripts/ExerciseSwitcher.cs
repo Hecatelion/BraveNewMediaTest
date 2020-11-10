1cd294ea90f5d2b4756e1bcf247cc52966cbb4c6
Baptiste <billard.baptiste.etudes@gmail.com>
2020-11-10 22:54:33 +0100

    Exercices Transition UI
    - curScene name displayed
    - on button click → load other exercise

---
+++ BNMUnityProject/Assets/Scripts/ExerciseSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExerciseSwitcher : MonoBehaviour
+{
+	// [SerializeField] Object sceneToLoad;
+	[SerializeField] string sceneToLoadName;
+	[SerializeField] Button button;
+	[SerializeField] Text curExText;
+
+	Text text;
+
+    void Start()
+    {
+		/*
+		button.GetComponentInChildren<Text>().text = "go to : " + sceneToLoad.name;
+		curExText.text = SceneManager.GetActiveScene().name;
+		*/
+
+		StartCoroutine(SetUITexts());
+    }
+
+    void Update()
+    { }
+
+	public void UI_SwichExercise()
+	{	
+		SceneManager.LoadScene(sceneToLoadName);
+	}
+
+	private IEnumerator SetUITexts() 
+	{
+		yield return new WaitForSeconds(0.01f);
+		curExText.text = SceneManager.GetActiveScene().name;
+		button.GetComponentInChildren<Text>().text = "go to : " + sceneToLoadName;
+	}
+}
